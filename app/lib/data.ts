5ba8af47384618c504372739245a25691e48cbcd
Valdean Souza <65917804+valdean132@users.noreply.github.com>
2024-09-15 16:25:33 -0400

    Parando na aula 10
    Aprendendo sobre Suspense e Loading e como aplicar cada um deles

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -6,7 +6,7 @@ import {
   InvoicesTable,
   LatestInvoiceRaw,
   Revenue,
-} from './definitions'
+} from '@/app/lib/definitions'
 import { formatCurrency } from './utils'
 
 export async function fetchRevenue() {
@@ -14,12 +14,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...')
+    await new Promise(resolve => setTimeout(resolve, 5000))
 
     const data = await sql<Revenue> /*sql*/`SELECT * FROM revenue`
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.')
 
     return data.rows
   } catch (error) {
